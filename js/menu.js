19b4d06c72b72dac978c33e5bb678c42fdd41a31
Jhonatan <multtii@outlook.com>
2025-10-02 14:56:41 -0300

    funcionalidade de abrir e fechar o menu

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const botaoMenu = document.querySelector(".menu-hamburguer");
+  const cabecalho = document.querySelector(".cabecalho");
+  botaoMenu.addEventListener("click", () => {
+    cabecalho.classList.toggle("menu-ativo");
+  });
+});
